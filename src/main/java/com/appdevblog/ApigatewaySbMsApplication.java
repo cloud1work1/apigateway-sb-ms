36a33098acaa55b3e7b122b566d1f6f11c2577b8
unknown <cloud1work1@gmail.com>
2021-01-08 13:10:19 +0530

    Initial Commit

---
+++ src/main/java/com/appdevblog/ApigatewaySbMsApplication.java
@@ -0,0 +1,13 @@
+package com.appdevblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApigatewaySbMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApigatewaySbMsApplication.class, args);
+	}
+
+}
